297fe5b2a048478726746b60632741078bb03148
William E. Alexander <williamericalexander@gmail.com>
2017-11-30 19:47:43 -0600

    pseudocode for controller and vehice methods

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 import time
+import queue
 
 #Controller class
 class Controller:
@@ -24,10 +25,12 @@ class Controller:
 		self.killed = True
 	
 class Phase:
-	elapsed = 0
-	min = 0
-	max = 0
-	extend = True
+	elapsed = 0.0
+	min = 0.0
+	max = 0.0
+	ext = True
+	vehicles = queue.Queue()
+	demand = 0.0
 	
 	id = None
 	def __init__(self,min, max, id):
@@ -37,15 +40,31 @@ class Phase:
 	
 	def start(self):
 		print("start of phase "+str(self.id))
-		self.elapsed = 0
+		self.elapsed = 0.0
 		for i in range(self.min):
 			time.sleep(1)
 			self.elapsed += 1
 			self.evaluateExtension()
-		while self.extend and self.elapsed < self.max:
-			time.sleep(1)
-			self.elapsed += 1
-			self.evaluateExtension()
+		while self.extend() and self.elapsed < self.max:
+			time.sleep(0.1)
+			self.elapsed += 0.1
+	
+	def extend(self):
+		# TODO: get positions of cars
 		
-	def evaluateExtension(self):
+		# TODO: get threshold size
+		
+		# TODO: use CFM to update all vehicle speeds
+		
+		# TODO: use previous speeds to update positions
+		
+		# TODO: use new positions to determine if there is a car inside threshold
+		
+		return self.ext
+
+class Generator:
+	def generateVehicles():
+		# Use random number generator to create new vehicles randomly according to demand w/ random attributes
+		
+		# Be sure to ensure that if the link is full that no new vehicles are generated
 		pass
--- vehicle.py
+++ vehicle.py
@@ -2,6 +2,18 @@
 class Vehicle:
 	position = None
 	velocity = None
-	acceleration = None
+	maxAcceleration = None
+	maxBraking = None
+	length = None
 	vehInFront = None
 	vehBehind = None
+	reactionTime = None
+	desiredSpeed = None
+	
+	def updatePosition(self):
+		# Update position based on v(t-1)
+		pass
+	
+	def updateVelocity(self):
+		# use car following model to determine new velocity
+		pass
